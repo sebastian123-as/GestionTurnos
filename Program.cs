20629b227837b6f1c1e7fe766859f18aadd7967b
sebastian123-as <camicarva119@gmail.com>
2024-04-24 16:47:52 -0500

    Enlace base de datos

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using Turnos.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BaseContext> (options => 
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("MySqlConnection"),
+        Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.20-mysql")
+    )
+);
 
 var app = builder.Build();
 
